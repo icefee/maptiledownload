b95ad3b4cde200d96b939bf201a28689eacb49b8
ying <icefee@live.cn>
2019-09-29 17:05:23 +0800

    Update download.js

--- download.js
+++ download.js
@@ -3,30 +3,20 @@ const https = require('https');
 const url = require('url');
 const io = require('./io');
 
-const home = `${__dirname}/tiles/`;
-
-module.exports = class DownloadTiles {
-	constructor({ theme, types }) {
+export class DownloadTiles {
+	constructor({ theme, types }, dir) {
 		this.theme = theme || '';
 		this.types = types || [];
 		this.downloading = false;
-		this.map_types = [
-			{
-				type: 'normal',
-				name: '街道图'
-			},
-			{
-				type: 'sate',
-				name: '卫星图'
-			}
-		]
+		this.dir = dir + '\\tiles\\';
 	}
 
 	tileUri(type, {x, y, z}) {
 		const baseUrl = 'https://gss0.bdstatic.com';
 		const tileTemplate = {
 			normal: `${baseUrl}/8bo_dTSlRcgBo1vgoIiO_jowehsv/tile/?qt=vtile&x=${x}&y=${y}&z=${z}&styles=pl&scaler=1&udt=20181030`,
-			sate: `${baseUrl}/5bwHcj7lABFT8t_jkk_Z1zRvfdw6buu/it/u=x=${x};y=${y};z=${z};v=009;type=sate&fm=46&udt=20181030`
+			sate: `${baseUrl}/5bwHcj7lABFT8t_jkk_Z1zRvfdw6buu/it/u=x=${x};y=${y};z=${z};v=009;type=sate&fm=46&udt=20181030`,
+			mix: `${baseUrl}/8bo_dTSlR1gBo1vgoIiO_jowehsv/tile/?qt=vtile&x=${x}&y=${y}&z=${z}&styles=sl&udt=20190726`,
 		}
 		let theme = this.theme;
 		if(theme != '') {
@@ -35,28 +25,18 @@ module.exports = class DownloadTiles {
 		return tileTemplate[type]
 	}
 
-	getTypeByName(name) {
-		let types = this.map_types;
-		let type = null;
-		for(let i in types) {
-			if(types[i].name == name) {
-				type = types[i].type
-			}
-		}
-		return type;
-	}
-
 	createUrls(points, types) {
-		if(!types.length) {
+		let mapTypes = types || this.types;
+		if(!mapTypes.length) {
 			return;
 		}
 		let urls = [];
-		for(let i = 0; i < types.length; i ++) {
-			let type = this.getTypeByName(types[i]);
+		for(let i = 0; i < mapTypes.length; i ++) {
+			let type = mapTypes[i];
 			for(let j = 0; j < points.length; j ++) {
 				let pt = this.clone(points[j]);
 				let url = this.tileUri(type, pt);
-				urls.push(Object.assign(pt, {type, url}))
+				urls.push({ ...pt, ...{ type, url } })
 			}
 		}
 		return urls
@@ -84,83 +64,55 @@ module.exports = class DownloadTiles {
 		})
 	}
 
-	downloadTile({x, y, z, type, url}, cb) {
-		let callbacks = {}
-	
-		callbacks.success = res => {
-			let buffers = Buffer.concat(res);
-
-			let typeDir = `${home}${type}`;
-			if(!io.exist(typeDir)) {
-				io.mkDir(typeDir)
-			}
-	
-			let zDir = `${typeDir}/${z}/`;
-			io.mkDir(zDir)
-	
-			let xDir = `${zDir}/${x}/`;
-			io.mkDir(xDir)
-	
-			// let yDir = `${xDir}${y}/`
-			// mkDir(yDir)
-	
-			io.writeFile(`${xDir}${y}.png`, buffers);
-			cb(true)
-		}
-	
-		callbacks.fail = err => {
-			//
-			cb(false)
-		}
-		
-		this.fetchData(url, callbacks)
-	}
-
-	getNow() {
-		return new Date().getTime()
-	}
-
-	download(list, cbs) {
-
-		console.log(`开始下载......`)
-	
-		let start = this.getNow();
-		
-		let index = 0;
-		let imgCount = list.length;
-		let run = () => {
-			if(!this.downloading) {
-				return;
-			}
-
-			this.downloadTile(list[index], res => {
-				if(res) {
-					cbs.process((index + 1)/imgCount);
-	
-					console.log(`正在下载${index + 1}/${imgCount}......`)
-					if(index < imgCount - 1) {
-						index ++;
-						run()
-					}
-					else {
-						let cost = this.getNow() - start;
-						console.log(`下载完成, 总共耗时${cost/1000}秒.`);
-						cbs.success()
-					}
+	downloadTile({ x, y, z, type, url }) {
+		return new Promise(resolve => {
+			let callbacks = {}
+			
+			callbacks.success = res => {
+				let buffers = Buffer.concat(res);
+				let typeDir = `${this.dir}${type}`;
+				if(!io.exist(typeDir)) {
+					io.mkDir(typeDir)
 				}
-			})
+				
+				let zDir = `${typeDir}\\${z}\\`;
+				io.mkDir(zDir)
+				
+				let xDir = `${zDir}\\${x}\\`;
+				io.mkDir(xDir)
+				
+				io.writeFile(`${xDir}${y}.png`, buffers);
+				resolve(1);
+			}
+			
+			callbacks.fail = err => {
+				resolve(0)
+			}
+			
+			this.fetchData(url, callbacks)
+		})
+	}
+
+	async download(list, cbs) {
+		let SUCCESS = 0, FAIL = 0;
+		if(!this.downloading) {
+			return;
 		}
-	
-		run()
+
+		for(let i = 0; i < list.length; i ++) {
+			if(!this.downloading) break;
+			let res = await this.downloadTile(list[i]);
+			res ? SUCCESS ++ : FAIL ++;
+			cbs.process((i + 1) / list.length);
+		}
+		cbs.success({ SUCCESS, FAIL });
+		this.downloading = false;
 	}
 
 	downloadTiles(points, cbs) {
-
 		this.downloading = true;
-
 		let urls = this.createUrls(points, this.types);
-	
-		io.mkDir(home)
+		io.mkDir(this.dir);
 		this.download(urls, cbs)
 	}
 
